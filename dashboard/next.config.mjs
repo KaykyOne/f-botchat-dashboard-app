bd459fb7d87fafb11ddb3284e2d41d72588a4584
KaykyOne <kaykyzioti@gmail.com>
2025-10-22 10:01:13 -0300

    Configure GitHub Pages deployment

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/n-dashboard-bot',
+  assetPrefix: '/n-dashboard-bot',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
