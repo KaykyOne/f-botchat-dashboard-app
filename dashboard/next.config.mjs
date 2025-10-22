2039eba0c1bc927934c09b259de05c5f27ae30c7
KaykyOne <kaykyzioti@gmail.com>
2025-10-22 10:24:22 -0300

    att path

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/n-dashboard-bot',
-  assetPrefix: '/n-dashboard-bot',
+  basePath: '/f-botchat-dashboard-app',
+  assetPrefix: '/f-botchat-dashboard-app',
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_APIURL: process.env.NEXT_PUBLIC_APIURL,
+    NEXT_PUBLIC_SUPABASE: process.env.NEXT_PUBLIC_SUPABASE,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
